037416e0d40efbdddad7541141ec8d03e1d28e6a
whipbaek <jibaek0513@gmail.com>
2022-05-23 19:02:55 +0900

    data parsing by classroom

--- src/Model/ModelTest.java
+++ src/Model/ModelTest.java
@@ -8,6 +8,41 @@ public class ModelTest {
         ITBuild itBuild = new ITBuild();
         itBuild.setArrayLists(); //파일에서 데이터 읽어옴
         itBuild.setCinfos(); // 읽어온 데이터를 Cinfos에 저장함
-        itBuild.showAllClassInfo(); // Cinfos 를 모두 출력
+      //  itBuild.showAllClassInfo(); // Cinfos 를 모두 출력
+
+       // HashMap<String, String> hashMap = new HashMap<>();
+
+        /**
+         *   Class 정보를 모두 돌면서 Key: 강의실 / Value : result String
+         *  전체 강의실 셋팅 및 출력
+        for (ClassInfo classinfo : itBuild.getClassInfos()) {
+            hashMap.put(classinfo.getClassroom(), itBuild.IsEmptyRoom(classinfo.getClassroom(),"월","12:00"));
+        }
+
+        for (String s : hashMap.keySet()) {
+            System.out.println("s = " + s);
+        }
+        */
+
+//        System.out.println(itBuild.IsEmptyRoom("301","월","12:00"));
+//
+//        for(ClassInfo classInfo : itBuild.getClassInfos()){
+//            System.out.println(classInfo.getClassroom());
+//            System.out.println(itBuild.IsEmptyRoom(classInfo.getClassroom(), "월","09:57"));
+//            System.out.println();
+//        }
+
+        itBuild.setClassByRoom();
+      itBuild.printClassByRoom();
+
+
+        for (String room : itBuild.getClassByRoom().keySet()) {
+            System.out.println("room : " + room);
+            System.out.println(itBuild.IsEmptyRoom2(room, "월", "11:11"));
+        }
+
+//        System.out.println(itBuild.IsEmptyRoom("355","목","15:00"));
+
+
     }
 }
---
+++ src/Model/TimeCalculator2.java
@@ -0,0 +1,122 @@
+package Model;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+public class TimeCalculator2 {
+
+	LocalTime nowTime; // 현재 시간
+	LocalTime startTime; // 시작 시간
+	LocalTime endTime; // 종료 시간
+	boolean isEmpty = true;
+	long LeftTime = 99999;
+
+	private String result="";
+
+	public TimeCalculator2() {}
+
+	public TimeCalculator2(String input, String start, String end){
+		nowTime = StringToLocal(input);
+		startTime = StringToLocal(start);
+		endTime = StringToLocal(end);
+	}
+
+	public TimeCalculator2(String input){
+		nowTime = StringToLocal(input);
+	}
+
+	public void setTimes(String start, String end){
+		startTime = StringToLocal(start);
+		endTime = StringToLocal(end);
+	}
+
+	// nowTime 을 String으로 입력받고, local변수로 초기화 해주는 생성자
+	// strattime과 endtime 을 string으로 받고 local변수로 변환해주는 메소드
+	// 현재 local 변수에 now, start, end, 를 가지고 포함되어 있는지 아닌지 t/f로 반환하는 메소드
+	// 남
+
+	public LocalTime StringToLocal(String time){
+		String[] arr = time.split(":");
+		int[] arrInt = new int[arr.length];
+		for(int i = 0; i< arr.length; i++) arrInt[i] = Integer.parseInt(arr[i]);
+
+		return LocalTime.of(arrInt[0],arrInt[1]);
+	}
+	
+	public void compareTime() {
+
+		//사용중일 때
+		if (nowTime.isAfter(startTime) && nowTime.isBefore(endTime)) {
+			result =  "Classroom is now using, left time for empty is : " + nowTime.until(endTime, ChronoUnit.MINUTES) + " minutes";
+			LeftTime = nowTime.until(endTime, ChronoUnit.MINUTES);
+			isEmpty = false;
+		}
+
+		//사용중이지 않을때 && 사용시작 시간이 나보다 뒤일때
+		else if (nowTime.isBefore(startTime)) {
+			if(LeftTime > nowTime.until(startTime, ChronoUnit.MINUTES)){
+				result = "Classroom is now empty, left time for next class is : " + nowTime.until(startTime, ChronoUnit.MINUTES) + " minutes";
+				LeftTime = nowTime.until(startTime, ChronoUnit.MINUTES);
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		LocalTime s1 = LocalTime.of(20,20);
+		LocalTime n1 = LocalTime.of(18,20);
+
+		long until = n1.until(s1, ChronoUnit.MINUTES);
+		System.out.println("until = " + until);
+
+	}
+
+
+	public LocalTime getNowTime() {
+		return nowTime;
+	}
+
+	public void setNowTime(LocalTime nowTime) {
+		this.nowTime = nowTime;
+	}
+
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public boolean isEmpty() {
+		return isEmpty;
+	}
+
+	public void setEmpty(boolean empty) {
+		isEmpty = empty;
+	}
+
+	public long getLeftTime() {
+		return LeftTime;
+	}
+
+	public void setLeftTime(long leftTime) {
+		LeftTime = leftTime;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+}
